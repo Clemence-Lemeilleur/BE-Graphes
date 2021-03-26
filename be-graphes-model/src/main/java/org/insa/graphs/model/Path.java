dd2a73142fd2534f96f3c6833099eed1d84f4d2a
Clémence <clemence.lemeilleur@icloud.com>
2021-03-26 13:21:56 +0100

    Update Path.java

--- be-graphes-model/src/main/java/org/insa/graphs/model/Path.java
+++ be-graphes-model/src/main/java/org/insa/graphs/model/Path.java
@@ -210,11 +210,14 @@ public class Path {
      * 
      * @return Total length of the path (in meters).
      * 
-     * @deprecated Need to be implemented.
      */
     public float getLength() {
         // TODO:
-        return 0;
+    	float length=0;
+    	for(Arc arc :  this.arcs) {
+    		length+= arc.getLength();
+    	}
+        return length;
     }
 
     /**
@@ -225,11 +228,11 @@ public class Path {
      * @return Time (in seconds) required to travel this path at the given speed (in
      *         kilometers-per-hour).
      * 
-     * @deprecated Need to be implemented.
      */
     public double getTravelTime(double speed) {
         // TODO:
-        return 0;
+    	double TravelTime=this.getLength()/speed;
+        return TravelTime;
     }
 
     /**
