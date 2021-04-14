4e13e424fcd6614c6d8ad550f7ae40a008dee78c
Clémence <clemence.lemeilleur@icloud.com>
2021-04-14 18:14:31 +0200

    séance

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,5 +1,10 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import org.insa.graphs.algorithm.utils.BinaryHeap;
+//import org.insa.graphs.model.Arc;
+import org.insa.graphs.model.*; //tous importer d'un coup
+//import org.insa.graphs.model.Graph;
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     public DijkstraAlgorithm(ShortestPathData data) {
@@ -8,9 +13,37 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     @Override
     protected ShortestPathSolution doRun() {
+    	
         final ShortestPathData data = getInputData();
         ShortestPathSolution solution = null;
+        boolean dest = false; // pour savoir si on est arrivé à destination ou pas
+        Graph graph = data.getGraph(); // on récupère le graph de data
+        int Taille = graph.size(); //pour avoir la taille du graphe de data
+        
         // TODO:
+   /* On crée un tableau de Label, avec leur ID : l'indice est le sommet, et la case contient son Label */
+        Label tabLab[] = new Label [Taille];
+   
+   /* On crée un tas de Label */
+		BinaryHeap<Label> tas = new BinaryHeap<Label>();
+	
+	/*On crée un tableau avec tout les prédecesseurs d'un sommet, on garde les arcs */
+		Arc[] predecessor = new Arc[Taille];
+		
+	/* Initialisation */
+		//On met tout les points à l'infini pour initialiser avec le constructeur
+		for (Node noeud : graph.getNodes()) {
+			tabLab[noeud.getId()] = new Label(noeud);
+		}
+		//On ajoute le sommet de départ
+		Label deb = new Label(data.getOrigin());
+		tabLab[deb.getSommet().getId()] = deb;
+		tas.insert(deb);
+		deb.setInTas();
+		deb.setCost(0);
+		
+		
+
         return solution;
     }
 
--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
@@ -1,17 +1,89 @@
 package org.insa.graphs.algorithm.shortestpath;
 
-public final class Label{
+import org.insa.graphs.model.Node;
+import org.insa.graphs.model.Arc;
+
+public class Label implements Comparable<Label> {
 	//On va faire un arbre de label afin de trouver directement le plus petit qui sera la racine
-	public int Sommet;
-	public boolean Marque;
-	public int Coût;
-	public int Père; //on préfèrera prendre l'arc pour retrouver le chemin à la fin
+	public boolean Marked;
+	public float Cost;
+	public Node Father; //on préfèrera prendre l'arc pour retrouver le chemin à la fin : on ne servira pas de ça au final 
+	public Node Sommet_Courant; 
+	public boolean Tas; //vrai si notre noeud est dans le tas
+	public Arc FatherArc;
 	
-	public void getCost() {
-		
-		return Coût;
+//----------------------------Constructeur:-------------------------------------
+	//Les noeuds des graphes sont numérotés de 0 à N-1
+	public Label(Node noeud){
+		this.Sommet_Courant = noeud;
+		this.Marked = false;
+		this.Cost = Float.POSITIVE_INFINITY;
+		this.Father = null; 
+		this.Tas = false;
+		this.FatherArc = null;
+	}
+	
+//-----------------------------Méthodes: (pas utile comme mes attributs sont en public)----------------------------------------
+	public Node getSommet() {
+		return this.Sommet_Courant;
+	}
+	
+	public float getCost() {
+		return this.Cost;
+	}
+	
+	public float getTotalCost() {
+		return this.Cost;
+	}
+	
+	/* Retourne true si le noeud a été marqué */
+	public boolean getMark() {
+		return this.Marked;
+	}
+	
+	public Node getFather() {
+		return this.Father;
+	}
+	
+	/* Retourne true si le noeud a été mis dans le tas */
+	public boolean getTas() {
+		return this.Tas;
+	}	
+	
+	public void setMark() {
+		this.Marked = true;
+	}
+	
+	public void setCost(float cout) {
+		this.Cost = cout;
+	}
+	
+	public void setFather(Node father) {
+		this.Father = father;
+	}
+	
+	public void setInTas() {
+		this.Tas = true;
+	}
+	
+//On est obligé de mettre une classe compareTo, puisque notre tas est implémenté d'une classe Compare
+	
+	/* On compare les coûts des Labels */
+	public int compareTo(Label autre) {
+		int compare;
+		if (this.getTotalCost() < autre.getTotalCost()) {
+			compare = -1;
+		}
+		else if (this.getTotalCost() == autre.getTotalCost()) {
+			compare = 0;
+		}
+		else {
+			compare = 1;
+		}
+		return compare;
 		
 	}
+	
 }
 
 
